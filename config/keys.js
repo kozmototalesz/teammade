8e1db10639c0e38c48ab0bcb61e2a6e1c2931181
Kozma Viktor <kozmaviktor@Kozmos-Air.lan>
2018-10-27 14:30:04 +0200

    Initalization

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI: "mongodb://kozmo:Alma1234@ds245885.mlab.com:45885/done"
+}
---
+++ routes/api/posts.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+//@route GET api/posts/test
+router.get('/test', (req,res) => res.json({msg:"Users work"}));
+
+module.exports=router;
